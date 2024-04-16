774cbb01f33b634e41f7922241ffc125890f740d
Sw3dishGo4t <anton.linell@hotmail.se>
2024-04-16 17:06:35 +0200

    Update test_create.py
    Integration testing

--- backend/test/integration/test_create.py
+++ backend/test/integration/test_create.py
@@ -4,11 +4,44 @@ from unittest.mock import patch
 from  src.util.dao import DAO
 
 @pytest.fixture
-@patch('src.util.dao.dotenv_values', autospec=True)
-@patch('src.util.dao.getValidator', autospec=True)
 def sut():
-    mockeddao = mock.MagicMock()
-    mockedsut = DAO(mockeddao)
-    
-    yield mockedsut
-    mockedsut.drop()
+    with patch('src.util.dao.getValidator', autospec=True) as mockedgetValidator:
+        mockedgetValidator.return_value = {
+            "$jsonSchema": {
+                "bsonType": "object",
+                "required": ["description"],
+                "properties": {
+                    "description": {
+                        "bsonType": "string",
+                        "description": "the description of a todo must be determined",
+                        "uniqueItems": True
+                    }, 
+                    "done": {
+                        "bsonType": "bool"
+                    }
+                }
+            } 
+        }
+        #mockeddao = mock.MagicMock()
+        mockedsut = DAO("Test")
+
+        yield mockedsut
+        mockedsut.drop()
+        return mockedsut
+
+@pytest.mark.integration
+@pytest.mark.parametrize('data, result', [({'description':'values', 'done': True}, {'description':'values', 'done': True})])
+def test_create(sut, data, result):
+    createData = sut.create(data)
+    assert '_id' in createData
+    assert createData == result
+
+{'description': 1, 'done': True}
+{'done': True}
+{'description': 'test', 'done': True}, {'description': 'test', 'done': True}
+@pytest.mark.integration
+@pytest.mark.parametrize('data', [({'description': 'test', 'done': True}), ({'done': True}), ({'description': True, 'done': True})])
+def test_create_2(sut, data):
+    with pytest.raises(Exception):
+        sut.create({'description': 'test', 'done': True})
+        sut.create(data)
