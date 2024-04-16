68fc1d595e3e30178f280e2b887e7921681886d6
Sw3dishGo4t <anton.linell@hotmail.se>
2024-04-16 15:12:26 +0200

    Update test_create.py
    Add more of the test

--- backend/test/integration/test_create.py
+++ backend/test/integration/test_create.py
@@ -1,2 +1,11 @@
 import pytest
 import unittest.mock as mock
+from unittest.mock import patch
+from  src.util.dao import DAO
+
+@pytest.fixture
+@patch('src.util.dao.dotenv_values', autospec=True)
+@patch('src.util.dao.getValidator', autospec=True)
+def sut():
+    mockeddao = mock.MagicMock()
+    return mockeddao
