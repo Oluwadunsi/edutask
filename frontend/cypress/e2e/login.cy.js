b20c9a3d347044320415ee4c77a9c244f583f1de
Oluwadunsi <eafeniyi@gmail.com>
2024-06-30 20:30:30 +0200

    Cypress test

--- frontend/cypress/e2e/login.cy.js
+++ frontend/cypress/e2e/login.cy.js
@@ -1,10 +1,8 @@
-
 describe('Logging into the system', () => {
   // define variables that we need on multiple occasions
   let uid // user id
   let name // name of the user (firstName + ' ' + lastName)
   let email // email of the user
-
 
   before(function () {
     // create a fabricated user from a fixture
@@ -20,7 +18,8 @@ describe('Logging into the system', () => {
           name = user.firstName + ' ' + user.lastName
           email = user.email
         })
-      })
+      });
+
 
   })
 
@@ -33,8 +32,7 @@ describe('Logging into the system', () => {
     .type(email)
 
     cy.get('form')
-      .submit()
-      
+      .submit()      
   })
 
   it('starting out on the landing screen', () => {
@@ -62,131 +60,6 @@ describe('Logging into the system', () => {
       .should('contain.text', 'Your tasks, ' + name)
   })
 
-  it('todo description empty', () => {
-    cy.get('.container-element').then(($num) => {
-      if ($num.length < 0) { // create a new task for testing purpose if one doesn't exsist.
-          cy.get('#title')
-          .type('Hello world!')
-
-          cy.get('#url')
-          .type('U_gANjtv28g')
-
-          cy.get('form')
-          .submit()
-
-        }
-    })
-
-    cy.get('.container-element > a') //get the a tag in div with classname container-element
-    .first() //get the first task
-    .click()
-
-    cy.get('form')
-    .find('input[type=text]')
-    .should('have.prop', 'value')
-    .should('be.empty')
-
-    cy.get('form')
-    .find('input[type=submit]')
-    .should('be.disabled')
-
-  })
-
-  it('todo description not empty', () => {
-    cy.get('.container-element').then(($num) => {
-      if ($num.length < 0) { // create a new task for testing purpose if one doesn't exsist.
-        cy.get('#title')
-        .type('Hello world!')
-
-        cy.get('#url')
-        .type('U_gANjtv28g')
-
-        cy.get('form')
-        .submit()
-
-      }
-    })
-    
-    //get the classname for the div and trigger an onclick
-    cy.get('.container-element > a') //get the a tag in div with classname container-element
-      .first() //get the first task
-      .click()
-
-     cy.get('input[placeholder*="Add a new todo item"]') //gets the elemenet with the mentioned placeholder
-       .type("Watch a video")  // adds a todo item 
-
-    cy.get('input[placeholder*="Add a new todo item"]')
-      .should('have.prop', 'value') // checks if value of the above placeholder
-      .should('not.be.empty')
-
-    cy.get('form')
-      .find('input[type=submit]')
-      .should('be.enabled')
-
-  })
-
-  it('todo-item done', () => {
-    //get the classname for the div and trigger an onclick
-    cy.get('.container-element > a') //get the a tag in div with classname container-element
-    .first() //get the first task
-    .click()
-
-    cy.get('input[placeholder*="Add a new todo item"]') //gets the elemenet with the mentioned placeholder
-    .type("Watch the video")  // adds a todo item 
-    
-    cy.get('form.inline-form') //get the form on the page with ".inline-form" classname
-    .submit()
-
-    cy.get('.todo-item .checker')
-    .first()
-    .should('have.class', 'unchecked') // todo-item should be unchecked before it can be set to done
-
-    cy.get('.todo-item .checker')
-    .first()
-    .click() //click on the todo-item and set it to done
-
-    cy.get('.todo-item .checker')
-    .first()
-    .should('have.class', 'checked')  //todo-item should now be set to done
-    
-    cy.get('.editable')
-    .first()
-    .invoke('css', 'text-decoration')
-    .should('include', 'line-through')  // text should be stricken if checked as done
-  })  
-
-  it('todo-item not done', () => {
-    //get the classname for the div and trigger an onclick
-    cy.get('.container-element > a') //get the a tag in div with classname container-element
-    .first() //get the first task
-    .click()
-
-    cy.get('.todo-item .checker')
-    .first()
-    .should('have.class', 'checked')  //todo-item should be set to checked(active)
-
-    cy.get('.todo-item .checker')
-    .first()
-    .click() //click on the todo-item again and set it to active
-
-    cy.get('.todo-item .checker')
-    .first()
-    .should('have.class', 'unchecked')  //todo-item should now be set to unchecked(active) after its clicked
-
-  }) 
-  
-
-  it('delete todo-item', () => {
-    //get the classname for the div and trigger an onclick
-    cy.get('.container-element > a') //get the a tag in div with classname container-element
-    .first() //get the first task
-    .click()
-
-    cy.get('.todo-item .remover') // get the element on page with the classname and click on it
-    .first()
-    .click()
-  })
-
 
   after(function () {
 
@@ -196,10 +69,7 @@ describe('Logging into the system', () => {
       url: `http://localhost:5000/users/${uid}`
     }).then((response) => {
       cy.log(response.body)
-    }) 
-
+    });
   })
+  
 })
-
-
-
